2bd03bdc89d99d75fb98eb428ab6ea1cb9421ad3
Xander Miller <xander.miller@gmail.com>
2019-09-11 15:00:58 -0400

    adds logout test and starts tagging tests

--- tests/Browser/admin-portal.testcafe.js
+++ tests/Browser/admin-portal.testcafe.js
@@ -1,10 +1,7 @@
 import { Selector } from "testcafe";
 import { adminUser } from "./helpers/roles";
 
-fixture(`Critical - Job Poster Builder`).page(`talent.test`);
-
-// Skip when writing new tests
-// fixture.skip(`Critical - Job Poster Builder`);
+fixture(`Login to Admin Portal`).page(`talent.test`);
 
 test("Login as Admin #critical", async t => {
     await t
@@ -16,4 +13,18 @@ test("Login as Admin #critical", async t => {
           Selector("h1").withText("Dashboard").visible,
         )
         .ok()
+  });
+
+fixture(`Logout of Admin Portal`).page(`talent.test/admin`);
+
+test("Logout as Admin #critical #focus", async t => {
+    await t
+        // Login as manager.
+        .useRole(adminUser)
+        // Go to Job Poster Builder.
+        .navigateTo("/admin")
+        //Log out
+        .click(Selector("a").withText("Logout"))
+    const location = await t.eval(() => window.location);
+    await t    .expect(location.pathname).eql("/admin/login")
   });
