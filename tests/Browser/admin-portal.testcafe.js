fc2c0c36c18cee95cd8c21ce0584337858fb7115
Xander Miller <xander.miller@gmail.com>
2019-09-17 14:51:57 -0400

    Adds navigate to users index test

--- tests/Browser/admin-portal.testcafe.js
+++ tests/Browser/admin-portal.testcafe.js
@@ -2,8 +2,8 @@ import { Selector } from "testcafe";
 import { adminUser } from "./helpers/roles";
 import AdminPage from "./helpers/pageModels/admin.pageModel";
 
-fixture(`Login to Admin Portal`).page(`talent.test`);
-test("Login as Admin #critical #focus", async t => {
+fixture(`Login to Admin Portal #admin`).page(`talent.test`);
+test("Login as Admin #critical", async t => {
   await t
     // Login as Admin.
     .useRole(adminUser)
@@ -13,14 +13,35 @@ test("Login as Admin #critical #focus", async t => {
     .ok();
 });
 
-fixture(`Logout of Admin Portal`).page(`talent.test/admin`);
+fixture(`Admin Portal Interface #admin`).page(`talent.test/admin`);
+
+test("Login as Admin #critical #focus", async t => {
+  await t
+    // Login as Admin.
+    .useRole(adminUser)
+    // Go to Job Poster Builder.
+    .navigateTo("/admin")
+    .click(AdminPage.usersTab)
+    .expect(AdminPage.usersTitle)
+    .ok();
+});
+
+fixture(`Admin Portal User Management #admin`).page(`talent.test/admin`);
+
+fixture(`Admin Portal Manager Management #admin`).page(`talent.test/admin`);
+
+fixture(`Admin Portal Job Poster Management #admin`).page(`talent.test/admin`);
+
+// TODO: Figure out why if logout test is first login test fails. *** JUST KEEPING LOGOUT TEST LAST *** for now.
+
+fixture(`Logout of Admin Portal #admin`).page(`talent.test/admin`);
 test("Logout as Admin #critical", async t => {
   await t
     // Login as Admin.
     .useRole(adminUser)
     // Go to Job Poster Builder.
     .navigateTo("/admin")
-    //Log out
+    // Log out
     .click(AdminPage.logoutButton);
   const location = await t.eval(() => window.location);
   await t.expect(location.pathname).eql("/admin/login");
--- tests/Browser/helpers/pageModels/admin.pageModel.js
+++ tests/Browser/helpers/pageModels/admin.pageModel.js
@@ -2,8 +2,10 @@ import { Selector } from "testcafe";
 
 class AdminPage {
   constructor() {
-    this.dashboardTitle = Selector("h1");
-    this.logoutButton =  Selector("a").withText("Logout");
+    this.dashboardTitle = Selector("h1").withText("Dashboard");
+    this.logoutButton = Selector("a").withText("Logout");
+    this.usersTab = Selector("a").withText("Users");
+    this.usersTitle = Selector("h1").withText("Users");
   }
 }
 
