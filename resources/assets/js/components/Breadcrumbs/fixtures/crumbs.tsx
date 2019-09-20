6037ea13b096a7157361f3e134009a6d01446274
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2019-09-20 15:40:26 -0400

    added dummy data

---
+++ resources/assets/js/components/Breadcrumbs/fixtures/crumbs.tsx
@@ -0,0 +1,17 @@
+import { BreadcrumbItemType } from "../Breadcrumbs";
+
+export const crumbs: BreadcrumbItemType[] = [
+  {
+    href: "#",
+    title: "Manager Portal",
+  },
+  {
+    href: "#",
+    title: "Job Poster Builder",
+  },
+  {
+    href: "#",
+    title: "Step 01 (Introduction)",
+    currentPage: true,
+  },
+];
